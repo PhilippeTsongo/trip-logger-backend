39401471b327c294fdb8264565e6e2495ad75510
philippetsongo <philippetsongo90@gmail.com>
2025-03-20 23:24:35 +0200

    Added Railway domain to ALLOWED_HOSTS

--- trip_planner/settings.py
+++ trip_planner/settings.py
@@ -30,8 +30,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "trip-logger-backend-production.up.railway.app",
+    "127.0.0.1",
+    "localhost"
+]
 
 # Application definition
 
